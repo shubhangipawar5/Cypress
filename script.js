5a2878f84ab0b5d1e0defef4b33fd5c6415a0f2e
shubhangipawar5 <shubhangi5693@gmail.com>
2020-10-03 14:53:24 +0530

    third commit added a object

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 document.querySelector('p').addEventListener('click',()=>{
     document.querySelector('p').style.backgroundColor='aqua'
 })
+var person={
+    name:"shubhangi",
+    age:27,
+    cook:function(){
+        console.log(this.name +"likes to cook")
+    }
+}
